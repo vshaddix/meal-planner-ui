f5d5913a70215487d856bb1925a37ec2c9b6f857
Vasil Shaddix <vasilshaddixx@gmail.com>
2019-05-14 14:58:00 +0300

    change the title to the website

--- src/container/App.js
+++ src/container/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h3>User Authentication System</h3>
+        <h3>Meal planning web software</h3>
         {this.props.children}
       </div>
     );
