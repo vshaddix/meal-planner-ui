3042499aa253354ddda0dd1abfa01531c6c03701
Vasil Shaddix <vasilshaddixx@gmail.com>
2019-05-14 14:49:13 +0300

    add login/register functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './store/configureStore';
+import routes from './routes';
+// import './index.css';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
