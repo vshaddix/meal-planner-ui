53d7e51003ba4c15fed270ed1ffb6f9fc388f846
Vasil Shaddix <vasilshaddixx@gmail.com>
2019-06-19 10:06:54 +0300

    add negative test for create categories actions

--- tests/actions/CategoriesActions.test.js
+++ tests/actions/CategoriesActions.test.js
@@ -4,8 +4,6 @@ import * as types from '../../src/actions'
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-
-
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
@@ -18,7 +16,7 @@ describe('Categories actions', () => {
     CategoryService.mockClear();
   });
 
-  it('should call the create category method and dispatch correct actions', async () => {
+  it('should call the create category method and dispatch correct actions when the service returns status success', async () => {
     const store = mockStore({});
     const responseMock = {
       status: 'success',
@@ -32,7 +30,7 @@ describe('Categories actions', () => {
 
     CategoryService.mockImplementation(() => {
       return {
-        createNew: (data) => {
+        createNew: () => {
           return new Promise(resolve => resolve(responseMock))
         }
       };
@@ -45,6 +43,38 @@ describe('Categories actions', () => {
       },
       {
         type: types.CREATE_CATEGORY,
+        payload: responseMock,
+      }
+    ];
+
+    await store.dispatch(actions.createCategory(category));
+
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+  it('should call the create category method and dispatch correct actions when the service returns status fail', async () => {
+    const store = mockStore({});
+    const responseMock = {
+      status: 'fail',
+      data: {}
+    };
+
+    const category = {
+      id: 1,
+      name: 'Breakfast'
+    };
+
+    CategoryService.mockImplementation(() => {
+      return {
+        createNew: () => {
+          return new Promise(resolve => resolve(responseMock))
+        }
+      };
+    });
+
+    const expectedActions = [
+      {
+        type: types.CREATE_CATEGORY_ERROR,
         payload: responseMock,
       }
     ];
