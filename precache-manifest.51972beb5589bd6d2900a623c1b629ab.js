d87dd1f03bc52cbf2737a1c80d24bda853baf224
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-10 08:12:48 +0000

    Deploy vshaddix/meal-planner-ui to github.com/vshaddix/meal-planner-ui.git:gh-pages

---
+++ precache-manifest.51972beb5589bd6d2900a623c1b629ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c459356563a1f5bbc70f3108428c8bd",
+    "url": "/meal-planner-ui/index.html"
+  },
+  {
+    "revision": "f603f3db8c59647fbc06",
+    "url": "/meal-planner-ui/static/css/main.ce890c72.chunk.css"
+  },
+  {
+    "revision": "1c2c033f885cc5db4244",
+    "url": "/meal-planner-ui/static/js/2.91a4cab6.chunk.js"
+  },
+  {
+    "revision": "f603f3db8c59647fbc06",
+    "url": "/meal-planner-ui/static/js/main.ad5df6c3.chunk.js"
+  },
+  {
+    "revision": "7279d2493b6f0b9e8f2b",
+    "url": "/meal-planner-ui/static/js/runtime~main.ec10e311.js"
+  }
+]);
